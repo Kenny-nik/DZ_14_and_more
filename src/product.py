2e360831b901765f4d9e030483bc8eba496e35de
Nikita <nikkitos.pershin.2000@mail.ru>
2024-10-20 21:23:30 +0500

    update main.py

--- src/product.py
+++ src/product.py
@@ -1,23 +1,32 @@
 class Product:
-    """
-    Класс для представления продукта.
-    """
-
-    name = str
-    description = str
-    price = float
-    quantity = int
-
-    def __init__(self, name, description, price, quantity):
-        """
-        Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
-        """
+    """Класс предоставляющий информацию о товаре, описании, цене и количестве"""
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, dict_product, products=None):
+        if products:
+            for product in products:
+                if product.name == dict_product['name']:
+                    product.quantity += dict_product['quantity']
+                    product.price = max([product.price, dict_product['price']])
+                    return product
+        return cls(dict_product['name'], dict_product['description'], dict_product['price'], dict_product['quantity'])
 
-product1 = Product("Product 1", "Description 1", 99.99, 10)
-product2 = Product("Product 2", "Description 2", 199.99, 5)
-# работаем
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: int):
+        if new_price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+            return
+        if new_price < self.__price:
+            answer = input('Введите "y", если согласны понизить цену, если не согласны введите "n"\n')
+            if answer != 'y':
+                return
+        self.__price = new_price
