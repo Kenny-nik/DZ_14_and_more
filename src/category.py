5e8a59e5f074587d2dac276a8b5cfbd13bdd9ac8
Nikita <nikkitos.pershin.2000@mail.ru>
2024-10-20 21:23:18 +0500

    update category.py

--- src/category.py
+++ src/category.py
@@ -1,32 +1,29 @@
-from src.product import product1, product2
-
+from src.product import Product
 
 class Category:
-    """
-    Класс для описания категории товара
-    """
-
-    name: str
-    description: str
-    products: list
+    """Класс предоставляющий информацию о категории товара, количестве товаров в категории и количестве категорий"""
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
-        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
+    def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
-        Category.product_count += len(self.products)
+        self.__products = products if products else []
+
+        Category.product_count += len(products) if products else 0
         Category.category_count += 1
 
-    def add_product(self, product):
-        self.products.append(product)
+    def add_product(self, product: Product):
+        self.__products.append(product)
         Category.product_count += 1
 
+    @property
+    def products(self):
+        products_str = ''
+        for product in self.__products:
+            products_str += f'{product.name}, {product.price} руб., Остаток: {product.quantity} шт.\n'
+        return products_str
 
-category1 = Category("Category 1", "Description of category 1", [])
-category1.add_product(product1)
-category1.add_product(product2)
-category2 = Category("Category 2", "Description of category 2", [])
-# работаем
+    @property
+    def list_product(self):
+        return self.__products
