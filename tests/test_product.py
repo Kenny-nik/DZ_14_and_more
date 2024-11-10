36f479aeeb326b9d676f2f7b6b788781ee0ef524
Nikita <nikkitos.pershin.2000@mail.ru>
2024-11-10 19:13:10 +0500

    fixes

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,25 +1,58 @@
-def test_init_product_1(product_1):
-    assert product_1.name == "Samsung Galaxy S23 Ultra"
-    assert product_1.description == "256GB, Серый цвет, 200MP камера"
-    assert product_1.price == 180000.0
-    assert product_1.quantity == 5
+from unittest.mock import patch
+
+import pytest
+
+from src.product import Product
 
 
-def test_init_product_2(product_2):
-    assert product_2.name == "Iphone 15"
-    assert product_2.description == "512GB, Gray space"
-    assert product_2.price == 210000.0
-    assert product_2.quantity == 8
+def test_products(product) -> None:
+    assert product.description == 'Я сама "вечность"'
+    assert product.name == "Nokia 3310"
+    assert product.price == 9.99
+    assert product.quantity == 1
+    assert str(product) == "Nokia 3310, 9.99 руб. Остаток: 1 шт."
 
 
-def test_init_product_3(product_3):
-    assert product_3.name == "Xiaomi Redmi Note 11"
-    assert product_3.description == "1024GB, Синий"
-    assert product_3.price == 31000.0
-    assert product_3.quantity == 14
+def test_new_product(new_product):
+    result = Product.new_product(new_product)
+    assert result.name == "Samsung Galaxy C23 Ultra"
+    assert result.description == "256GB, Серый цвет, 200MP камера"
+    assert result.price == 180000.0
+    assert result.quantity == 5
 
 
-def test_price_update(capsys, product_1):
-    product_1.price = -100
+def test_new_price(product):
+    product.price = 100.0
+    assert product.price == 100
+
+
+def test_new_price_negative(capsys, product):
+    product.price = -1
     message = capsys.readouterr()
-    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+    assert message.out.strip().split("\n")[-1] == "Цена не должна быть нулевая или отрицательная"
+
+
+@patch("builtins.input", side_effect="y")
+def test_new_price_low(mock, product):
+    product.price = 5
+    assert product.price == 5
+
+
+def test_add(product2, product3):
+    assert product2 + product3 == 2580000.0
+
+
+def test_add_error(smartphone1, lawn_grass_1):
+    with pytest.raises(TypeError):
+        smartphone1 + lawn_grass_1
+
+
+def test_add_products(new_product):
+    products = [
+        Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 100000.0, 5),
+        Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+    ]
+    result = Product.new_product(new_product, products)
+    assert result.quantity == 10
+    assert result.price == 180000.0
+    assert result.name == "Samsung Galaxy C23 Ultra"
